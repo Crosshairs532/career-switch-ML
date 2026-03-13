102d52fc2e424c63cdbcbfbd8cfc36ceb2af159d
Crosshairs532 <samsularefin532@gmail.com>
2026-03-13 17:46:14 +0600

    Data Ingestion Started ..

---
+++ src/constants/__init__.py
@@ -0,0 +1,18 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+from datetime import date
+
+PIPELINE_NAME: str = ""
+ARTIFACT_DIR: str = "artifact"
+FILE_NAME: str = "data.csv"
+TRAIN_FILE_NAME: str = "train.csv"
+TEST_FILE_NAME: str = "test.csv"
+
+# Data Ingestion 
+
+DATA_INGESTION_COLLECTION_NAME: str = "career_switch"
+DATA_INGESTION_DIR_NAME: str = "data_ingestion"
+DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
+DATA_INGESTION_INGESTED_DIR: str = "ingested"
+DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.25
