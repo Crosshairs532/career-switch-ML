bc1185fcbac8008c89f3b6fdbfa176380e304724
Crosshairs532 <samsularefin532@gmail.com>
2026-03-15 02:21:43 +0600

    Data Transformation DONE

--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -8,6 +8,7 @@ ARTIFACT_DIR: str = "artifact"
 FILE_NAME: str = "data.csv"
 TRAIN_FILE_NAME: str = "train.csv"
 TEST_FILE_NAME: str = "test.csv"
+PREPROCSSING_OBJECT_FILE_NAME = "preprocessing.pkl"
 
 # Database
 MONGODB_URI=os.getenv("MONGODB_URI")
@@ -25,3 +26,9 @@ DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.25
 # Data Validation
 DATA_VALIDATION_DIR_NAME: str = "data_validation"
 DATA_VALIDATION_REPORT_FILE_NAME: str = "report.yaml"
+
+# Data Transformation
+
+DATA_TRANSFORMATION_DIR_NAME: str = "data_transformation"
+DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR: str = "transformed"
+DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR: str = "transformed_object"
--- src/utils/main_utils.py
+++ src/utils/main_utils.py
@@ -1,7 +1,13 @@
 from src.logger import get_logger
 from src.exception import CustomException
+import numpy as np
 import yaml
 import sys
+import inspect
+import os
+import dill
+
+
 logger = get_logger(__name__)
 
 def read_yaml_file(file_path: str) -> dict:
@@ -12,3 +18,28 @@ def read_yaml_file(file_path: str) -> dict:
         
     except Exception as e:
         raise CustomException(e, sys) from e
+
+
+def save_object(file_path: str, obj: object) -> None:
+    funct_name = inspect.currentframe().f_code.co_name
+    logger.info(f"Entered {funct_name}")
+
+    try:
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        with open(file_path, "wb") as file_obj:
+            dill.dump(obj, file_obj)
+
+        logger.info(f"Exited {funct_name}")
+
+    except Exception as e:
+        raise CustomException(e, sys) from e
+
+def save_numpy_array_data(file_path: str, array: np.array):
+    
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        with open(file_path, 'wb') as file_obj:
+            np.save(file_obj, array)
+    except Exception as e:
+        raise CustomException(e, sys) 
